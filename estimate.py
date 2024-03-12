d201ac0dd97e8f60fd3f05e1bfd667d7c6dd3f8b
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2024-03-12 13:22:38 +0000

    config, report, gitignore
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- estimate.py
+++ estimate.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 # Required Libraries.
+import csv
 import json
 import logging
 import os
 import sys
+import time
+import datetime
 import traceback
 import requests
 
 
-# pod-hour calculation as per doc
+# POD-hour calculation as per doc
 # https://docs.dynatrace.com/docs/shortlink/dps-containers#billing-granularity-for-pod-hour-consumption
 
-# gibHour calculation  
+# GiBHour calculation  
 # https://docs.dynatrace.com/docs/manage/subscriptions-and-licensing/dynatrace-platform-subscription/host-monitoring#gib-hour-calculation-for-containers-and-application-only-monitoring
 
 
@@ -22,15 +25,19 @@ min_memory = 0.25
 
 LOG_FILE = config['log_file']
 LOG_DIR = config['log_dir']
+REPORT_DIR = config['report_dir']
 
 TENANT_URL = config['tenant_url']
 API_TOKEN = config['api_token']
 
+
+unit=config['query']['unit']
+resolution=config['query']['resolution']
+from_timeframe=config['query']['from']
 #
 # Query to fetch the avg size of K8s PGIs and the ammount of datapoints with a 
 # 15m resolution 
-#
-query="""/api/v2/metrics/query?metricSelector=
+query_body="""/api/v2/metrics/query?metricSelector=
 builtin:tech.generic.mem.workingSetSize:parents:filter(
 and(
 in("dt.entity.process_group_instance",entitySelector("type(PROCESS_GROUP_INSTANCE),
@@ -48,9 +55,15 @@ or(
 in("dt.entity.host",entitySelector("type(HOST),softwareTechnologies(KUBERNETES)")),
 in("dt.entity.host",entitySelector("type(HOST),paasVendorType(KUBERNETES)")),
 in("dt.entity.host",entitySelector("type(HOST),paasVendorType(OPENSHIFT)")))
-)):splitBy("dt.entity.process_group_instance"):avg:fold(avg):sort(value(avg,ascending)):toUnit(Byte,GigaByte)
-&resolution=15m&from=now-27d
+)):splitBy("dt.entity.process_group_instance"):avg:fold(avg):sort(value(avg,ascending))
 """
+q_to_unit=":toUnit(Byte," + unit + ")"
+q_resolution="&resolution="+ resolution
+q_from="&from=" + from_timeframe
+
+# Put the parametrized Wuery together
+query = query_body + q_to_unit + q_resolution + q_from
+
 class EmptyResponse:
     """Set an empty response with code"""
     status_code = 500
@@ -90,6 +103,14 @@ class PGI:
         self.memory_total = (self.memory_rounded / 4) * self.count
         logging.debug("%s", self)
 
+    def get_list_values(self):
+        """Returns the list of values"""
+        return [self.id, self.count ,self.memory,self.memory_rounded,self.memory_total]
+    
+    def get_list_header(self):
+        """Returns the headers as strings"""
+        return ["PGI", "Count" ,"Memory AVG","Memory Rounded","Memory Total"]
+    
     def __str__(self):
         return f"{self.id}, count({self.count}), mem({self.memory}), rounded({self.memory_rounded}), total({self.memory_total})"
 
@@ -102,6 +123,8 @@ def check_create_dir(dir_name):
 # Logging configuration
 # Create log directory at initialization
 check_create_dir(LOG_DIR)
+
+check_create_dir(REPORT_DIR)
 
 # Initialize logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -123,6 +146,10 @@ def verify_request():
     """Verify request"""
     return True
 
+def get_now_as_string():
+    ts = time.time()
+    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+
 def do_get(endpoint):
     """Function get http request"""
     response = requests.get(TENANT_URL + endpoint, headers=get_header(), verify=verify_request(), timeout=10)
@@ -142,36 +169,38 @@ def validate_set_action_status(response, action, defaultvalue=''):
         action_status = True
     else:
         result = str(response.status_code)
-        logging.warning(action + ':\t: code:' + result +
+        logging.warning(action + ':\t code:' + result +
                         ' reason:' + response.reason + ' Content:' + str(response.content))
         action_status = False
 
-    logging.info(action + ':\t:' + result)
-    logging.debug(action + ':\t:' + result + 'Content:' + str(response.content))
+    logging.info(action + ':\t' + result)
+    logging.debug(action + ':\t' + result + 'Content:' + str(response.content))
     return action_status
 
 def estimate_costs():
     """Function to estimate the costs"""
-    logging.info("----------- Estimating Kubernetes costs ------------------------")
-    
+    logging.info("Fetching all PGI datapoints in %s intervals and their AVG Memory from the API...", resolution)
     response = do_get(query)
     
     # Validate response
-    if not validate_set_action_status(response, 'query builtin:tech.generic.mem.workingSetSize'):
+    if not validate_set_action_status(response, 'Query [builtin:tech.generic.mem.workingSetSize] status'):
         return
 
-    #
     json_payload = json.loads(response.text)
     totalCount = json_payload['totalCount']
     nextPageKey = json_payload['nextPageKey']
-    resolution = json_payload['resolution']
+    response_resolution = json_payload['resolution']
     dataPointCountRatio = json_payload['result'][0]['dataPointCountRatio']
     dimensionCountRatio = json_payload['result'][0]['dimensionCountRatio']
-    logging.info("Total Count:%s, resolution:%s, dataPointCountRatio:%s, dimensionCountRatio:%s, nextPageKey:%s",totalCount,resolution, dataPointCountRatio, dimensionCountRatio, nextPageKey)
-    # TODO Validate Resolution of 15m
-    # TODO Check for Warnings
-    # TODO Calculate ratios?
-    # TODO Get Names
+    logging.info("Total Count:%s, response_resolution:%s, dataPointCountRatio:%s, dimensionCountRatio:%s, nextPageKey:%s",totalCount,response_resolution, dataPointCountRatio, dimensionCountRatio, nextPageKey)
+    # TODO Validate Resolution of 15m, change logic?
+    if resolution != response_resolution:
+        logging.warning("The response resolution of %s does not match the requested resolution %s", response_resolution, resolution)
+
+    # TODO Check for Warnings in response
+    # TODO Do we need to calculate ratios?
+    # TODO Get Names in the payload
+    # TODO Do a report
     total_memory = 0
 
     # Add the PGIs with the count
@@ -181,41 +210,69 @@ def estimate_costs():
         count = line['values'][0]
         pgis[pgi_id]= PGI(pgi_id, count, 0);
     
-    
-
     # Add and calculate the Memory
     for line in json_payload['result'][1]['data']:
         pgi_id = line['dimensions'][0]
         mem = line['values'][0]
         pgi = pgis[pgi_id]
         pgi.set_memory(mem)
-
         total_memory = total_memory + pgi.get_memory_total()
 
+    logging.info("Total Memory is %s GiB-hour for %s pod instances", total_memory, len(pgis))
 
-    logging.info("Total memory is %s GiB-hour for %s pod instances", total_memory, len(pgis))
+    write_report(pgis, total_memory)
     
     return 
 
+def write_report(pgis, total_memory):
+
+    filename = get_now_as_string() + "_k8s_estimation_costs_report_.csv"
+    logging.info("Writing a report in %s file", filename)
+
+    # Get the header of the first PGI Object in the dictionary
+    header = list(pgis.values())[0].get_list_header()
+    rows = []
+    for pgi in pgis.values():
+        rows.append(pgi.get_list_values())
+
+    delimiter = ["______", "______" ,"______","______", "______"]
+    summary = [len(pgis), " " ,"","", total_memory]
+ 
+    # writing to csv file
+    with open(REPORT_DIR + "/" + filename, 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+        # writing the fields
+        csvwriter.writerow(header)
+        # writing the data rows
+        csvwriter.writerows(rows)
+        # delimiter
+        csvwriter.writerow(delimiter)
+        # write the summary
+        csvwriter.writerow(summary)
+
+    return
 
 def main():
     try:
         printUsage = False
 
-        logging.info("----------------------------------------------")
+        logging.info("=================================================")
         logging.info("Starting Dynatrace Kubernetes License Estimation\n")
-
+        logging.info("=================================================")
+        logging.info("")
+        logging.info("Fetching all pod instances that have run in Kubernetes or ")
+        logging.info("Openshift environments for the timeframe of %s" , from_timeframe)
+        logging.info("")
+        logging.info("-------------------------------------------------")
+        logging.info("Tenant: %s", TENANT_URL)
+        logging.info("Log file: %s", LOG_FILE)
         estimate_costs()
 
     except Exception as e:  # catch all exceptions
         exc_type, exc_value, exc_traceback = sys.exc_info()
         logging.error(traceback.print_exc(), e)
         traceback.print_exc()
-        # Save it in object
-        #save_json(DATA_DIR, "error-results", CSV_DATA)
-        # Write Backup
-        #save_results(CSV_TEMP_DIR + '/error-' +
-         #            getNowAsString() + '-' + CSV_FILE)
 
     if printUsage:
         doUsage(sys.argv)
@@ -228,11 +285,7 @@ def get_usage_as_string():
     return """
 Dynatrace Kubernetes License Estimation 
 ================================================================
-Usage: estimate.py [command] [options]
-
-Help Commands:
- help           Prints this options
- validate       Validates and prints the config file, tests tests...
+Usage: estimate.py
 
 *** For more information read the README.md file ***
 ================================================================
