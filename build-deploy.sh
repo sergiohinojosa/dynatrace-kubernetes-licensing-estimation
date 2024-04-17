7a20a90f209f6ec4ac8980fd8a0b6760e3e6f12a
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2024-04-17 12:28:12 +0100

    better UX/UI
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
- docker build --tag shinojosa/k8stimator:0.2 . 
+ docker build --tag shinojosa/k8stimator:0.3 . 
 
- docker image push shinojosa/k8stimator:0.2
+ docker image push shinojosa/k8stimator:0.3
 
-kubectl set image deployment/k8stimator k8stimator=shinojosa/k8stimator:0.2 -n k8stimator
+kubectl set image deployment/k8stimator k8stimator=shinojosa/k8stimator:0.3 -n k8stimator
